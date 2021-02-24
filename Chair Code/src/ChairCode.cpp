337a6c1b2e232da55e2b8fbf2bbf8b7814a66b83
vanden46 <vanden46@gmail.com>
2021-02-24 17:55:02 -0500

    created arduino file and created the Resistance to PSI method
    created arduino file and created the Resistance to PSI method. needs a library to allow for more accurate calculations

---
+++ Chair Code/src/ChairCode.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+
+//variables for resistance to PSI method
+double multiplyer = 103194.978229681;
+double exponent = 1/-0.38226060803433;
+
+
+double ResistanceToPSI(double R){
+  double P;
+  P=R/multiplyer;
+  
+  P=pow(P, exponent);
+  return P;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(ResistanceToPSI(30000));
+  delay(100);
+}
