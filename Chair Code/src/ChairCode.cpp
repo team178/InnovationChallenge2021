849bfb855c9b1f26aa53b5a3c214ac71886f3587
vanden46 <vanden46@gmail.com>
2021-02-25 11:49:15 -0500

    added readResistance method
    this can read the resistance on the analog pin giving us the numbers we need for the ResistanceToPSI method

--- Chair Code/src/ChairCode.cpp
+++ Chair Code/src/ChairCode.cpp
@@ -4,22 +4,45 @@
 double multiplyer = 103194.978229681;
 double exponent = 1/-0.38226060803433;
 
+//variables for readResistance method
+int pullDownResistor=10000;
+int VCC = 5000;//done in mV so that I dont need to worry about decmils yet
+int fsrReading=0;
+int fsrVoltage=0;
 
+
+//will output the weight on the sensor from the resistance
 double ResistanceToPSI(double R){
-  double P;
-  P=R/multiplyer;
+  //n is exponent^-1
+  //formula is P=nroot(R/multiplyer)
+  //modified formula for code P=(r/multoplyer)^(1/n)
+  double PSI;
+  PSI=R/multiplyer;
   
-  P=pow(P, exponent);
-  return P;
+  PSI=pow(PSI, exponent);
+  return PSI;
+}
+
+//wull output the resistance on a certin pin
+int readResistance(int numPin){
+  int resistance;
+  fsrReading=analogRead(numPin);
+  fsrVoltage=map(fsrReading, 0,1023, 0, 5000);
+  if(fsrReading==0){
+    resistance=10000000;
+  }
+  else{
+    resistance=((VCC-fsrVoltage)*pullDownResistor)/fsrVoltage;
+  }
+
+  return resistance;
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Serial.println((double)ResistanceToPSI(30000));
   delay(100);
 }
